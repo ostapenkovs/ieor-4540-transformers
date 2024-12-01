640c9dae0efb437d9df16ca2f79663f6cf6bcef1
Vasiliy Ostapenko <ostapenkovs@pm.me>
2024-12-01 10:38:15 -0800

    added back transforms

--- src/util.py
+++ src/util.py
@@ -9,19 +9,19 @@ def get_data(dataset_name, train_dir, test_dir, dwn):
     if dataset_name == 'cifar10':
         my_dataset = datasets.CIFAR10
         channels, image_size, num_classes = 3, 32, 10
-        # transform = [
-        #     transforms.RandomCrop(image_size, padding=4),
-        #     transforms.RandomHorizontalFlip(),
-        #     transforms.RandomRotation(10)
-        # ]
+        transform = [
+            transforms.RandomCrop(image_size, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10)
+        ]
     elif dataset_name == 'mnist':
         my_dataset = datasets.MNIST
         channels, image_size, num_classes = 1, 28, 10
-        # transform = [
-        #     #
-        # ]
+        transform = [
+            #
+        ]
     
-    transform = [transforms.ToTensor(), transforms.Normalize([0.5]*channels, [0.5]*channels)]
+    transform += [transforms.ToTensor(), transforms.Normalize([0.5]*channels, [0.5]*channels)]
     transform = transforms.Compose(transform)
     train_set = my_dataset(root=train_dir, train=True, download=dwn, transform=transform)
     test_set  = my_dataset(root=test_dir, train=False, download=dwn, transform=transform)
